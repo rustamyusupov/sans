e99d3622f315f8b9f8ab52822756e1eb60dd3a74
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-09-15 13:35:30 +0200

    feat(bin): output usage information

--- src/bin/sans.js
+++ src/bin/sans.js
@@ -1,5 +1,29 @@
 #!/usr/bin/env node
 
-const run = () => {};
+import program from 'commander';
+
+import { version } from '../../package.json';
+import sans from '..';
+
+const run = () => {
+  program
+    .description('set StAtus iN Slack from the command line interface')
+    .version(version)
+    .option('-m, --message [text]', 'add text to the status', '')
+    .option('-e, --emoji [type]', 'add emoji to the status', '')
+    .option('-p, --presence', 'set precense away (default: "auto")')
+    .option('-d, --duration [minutes]', 'clear after time frame', 0)
+    .parse(process.argv);
+
+  const { message, emoji, presence, duration } = program;
+
+  if (!(message || emoji || presence)) {
+    program.outputHelp();
+
+    return;
+  }
+
+  sans({ message, emoji, presence, duration });
+};
 
 run();
