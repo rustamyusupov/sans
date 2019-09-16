b28880e4f730104fe46980227f8a1e601f2bcacf
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-09-16 20:45:53 +0200

    feat(index): set status to slack and return result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-export default ({ message, emoji, presence, duration }) => {
-  console.log(message, emoji, presence, duration);
+import setStatus from './slack';
+import getExpiration from './utils';
+
+export default async ({ message, emoji, duration }) => {
+  const token = process.env.SLACK_TOKEN;
+
+  if (!token) {
+    throw new Error('undefined SLACK_TOKEN in environment variables');
+  }
+
+  const expiration = getExpiration(duration);
+  const { ok, error } = await setStatus({ token, message, emoji, expiration });
+
+  console.log({ ok, error });
 };
