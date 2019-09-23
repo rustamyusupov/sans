d4a78f31b7bb2237dc016d71ef939726543be15d
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-09-23 21:47:54 +0200

    feat(bin): add presence to help

--- src/bin/sans.js
+++ src/bin/sans.js
@@ -12,11 +12,12 @@ const run = () => {
     .option('-m, --message [text]', 'add text to the status', '')
     .option('-e, --emoji [type]', 'add emoji to the status', '')
     .option('-d, --duration [minutes]', 'clear after time frame', 0)
+    .option('-p, --presence', 'set presence to away')
     .parse(process.argv);
 
-  const { message, emoji, duration } = program;
+  const { message, emoji, duration, presence } = program;
 
-  if (!message && !emoji) {
+  if (!message && !emoji && !presence) {
     program.outputHelp();
 
     return;
