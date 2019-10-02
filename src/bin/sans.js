7d0a97d9d9e602bf71ecfefd06a28228f24d8eea
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-10-02 22:06:10 +0200

    fix(bin): set message, emoji and duration as options with values

--- src/bin/sans.js
+++ src/bin/sans.js
@@ -9,15 +9,16 @@ const run = () => {
   program
     .description('set StAtus iN Slack from the command line interface')
     .version(version)
-    .option('-m, --message [text]', 'add text to the status', '')
-    .option('-e, --emoji [type]', 'add emoji to the status', '')
-    .option('-d, --duration [minutes]', 'clear message and emoji after time frame', 0)
+    .option('-m, --message <text>', 'add text to the status')
+    .option('-e, --emoji <type>', 'add emoji to the status')
+    .option('-d, --duration <minutes>', 'clear message and emoji after time frame')
     .option('-p, --presence <auto|away>', 'set presence')
     .parse(process.argv);
 
   const { message, emoji, duration, presence } = program;
+  console.log({ message, emoji, duration, presence });
 
-  if (!message && !emoji && !presence) {
+  if (message === undefined && emoji === undefined && presence === undefined) {
     program.outputHelp();
 
     return;
