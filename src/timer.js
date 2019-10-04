652a60883df846a5e9e15d0345dd0cf7563ed6b7
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-10-04 22:35:53 +0200

    feat(timer): add getendtime and settimer

---
+++ src/timer.js
@@ -0,0 +1,35 @@
+const secondsInMinute = 60;
+const milisecondsInSecond = 1000;
+
+export const getExpiration = duration => {
+  if (!duration) {
+    return 0;
+  }
+
+  const durationInSeconds = duration * secondsInMinute;
+  const nowInSeconds = Math.floor(Date.now() / milisecondsInSecond);
+
+  return nowInSeconds + durationInSeconds;
+};
+
+export const getEndTime = expiration => {
+  const epoch = new Date(0);
+  const date = new Date(epoch.setUTCSeconds(expiration));
+  const endTime = date
+    .toISOString()
+    .replace(/T/, ' ')
+    .replace(/\..+/, '');
+
+  return endTime;
+};
+
+export const setTimer = (expiration, cb) => {
+  const id = setInterval(() => {
+    const nowInSeconds = Math.floor(Date.now() / milisecondsInSecond);
+
+    if (nowInSeconds > expiration) {
+      cb();
+      clearInterval(id);
+    }
+  }, milisecondsInSecond);
+};
