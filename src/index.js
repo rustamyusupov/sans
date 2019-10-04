860748e7498ad9b479619c00b6678fb554df490d
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-10-04 22:36:48 +0200

    feat(index): show expiration notification, remove status by expiration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { setStatus, setPresence } from './slack';
-import getExpiration from './utils/getExpiration';
+import { getExpiration, getEndTime, setTimer } from './timer';
 
 export default ({ message, emoji, duration, presence }) => {
   const token = process.env.SLACK_TOKEN;
@@ -8,13 +8,23 @@ export default ({ message, emoji, duration, presence }) => {
     throw new Error('undefined SLACK_TOKEN in environment variables');
   }
 
-  if (message !== undefined || emoji !== undefined) {
-    const expiration = getExpiration(duration);
+  const expiration = getExpiration(duration);
+  const endTime = getEndTime(expiration);
 
+  if (message !== undefined || emoji !== undefined) {
     setStatus({ token, message, emoji, expiration });
   }
 
   if (presence) {
     setPresence({ token, presence });
+  }
+
+  if (expiration) {
+    console.log(`status set, status will be removed at ${endTime}`);
+
+    setTimer(expiration, () => {
+      setStatus({ token, message: '' });
+      setPresence({ token, presence: 'auto' });
+    });
   }
 };
